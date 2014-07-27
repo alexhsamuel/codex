a052cde03cb517c3195534b2afdfe0815bddcb42
Alex Samuel <alex@alexsamuel.net>
2014-07-27 14:06:42 -0400

    Other exercises.

---
+++ exercises/spl.py
@@ -0,0 +1,9 @@
+def split(text, sep):
+    '''
+    Divides up some a string 'text' at each occurrence of a separator 'sep'.
+
+    Returns a list of parts of the text, with the separators removed.
+    '''
+    # FIXME: Write me!
+
+
